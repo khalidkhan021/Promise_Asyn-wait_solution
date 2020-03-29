9d855b5ff32ed7ea067eac72b1d13e28e76f15d7
Khalid Mehfooz Khan <www.khalidkhan89@gmail.com>
2020-03-29 16:15:57 +0500

    what are the benefit of asyn/awit ??

--- Promise_Asyn-wait_solution.js
+++ Promise_Asyn-wait_solution.js
@@ -1,4 +1,4 @@
-
+//these approaches as callback
 // getCustomer(1, (customer) => {
 //   console.log('Customer: ', customer);
 //   if (customer.isGold) {
@@ -11,19 +11,7 @@
 //   }
 // });
 
-async function notifyCustomer() {
-  const customer = await getCustomer(1);
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    const movies = await getTopMovies();
-    console.log('Top movies: ', movies);
-    await sendEmail(customer.email, movies);
-    console.log('Email sent...');
-  }
-}
-notifyCustomer();
-
-
+//step1: create function using promise
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -52,3 +40,23 @@ function sendEmail(email, movies) {
     }, 4000);
   });
 }
+
+//step2: create function using asyn/awit as below:
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
+  console.log('Customer: ', customer);
+  if (customer.isGold) {
+    const movies = await getTopMovies();
+    console.log('Top movies: ', movies);
+    await sendEmail(customer.email, movies);
+    console.log('Email sent...');
+  }
+}
+notifyCustomer();
+
+//what are the benefit of asyn/awit ??
+//Its helps to  never used nested ".then", 
+//means getCustomer(1).then({res=>getTopMovie(res).then(res=>sendEmail(customer.email, movies).then(call catch))}}).......etc
+
+
+
